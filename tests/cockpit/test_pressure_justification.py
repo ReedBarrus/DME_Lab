ce88d9b57187b69822401d5faf380e2f096ac13a
Reed Barrus <reedbarrus@gmail.com>
2026-09-25 17:04:19 -0700

    Test planner routes through materialized-unit authority pressure

--- tests/cockpit/test_pressure_justification.py
+++ tests/cockpit/test_pressure_justification.py
@@ -315,6 +315,26 @@ class PressureJustificationTests(unittest.TestCase):
         )
 
 
+    def test_materialized_unit_authority_binding_is_selected_before_loop(self):
+        workcycle = workcycle_fixture("AUTO_CONTINUATION_PRESSURE")
+        workcycle["campaign_progress"]["T7"] = {"posture": "BOUNDED_PASS"}
+        result = build_pressure_justification(
+            workcycle=workcycle,
+            horizon_closure=horizon_fixture(),
+            qualification=qualification_fixture(
+                self_moving=[
+                    "MATERIALIZED_UNIT_AUTHORITY_BINDING:UNFROZEN",
+                    "REPEATED_METABOLIC_LOOP:UNFROZEN",
+                ],
+            ),
+        )
+        self.assertEqual(result["pressure_posture"], "JUSTIFIED")
+        self.assertEqual(
+            result["proposed_pressure"],
+            "MATERIALIZED_UNIT_AUTHORITY_BINDING_PRESSURE",
+        )
+
+
     def test_repeated_metabolic_loop_is_selected_after_authority_binding(self):
         workcycle = workcycle_fixture("AUTO_CONTINUATION_PRESSURE")
         workcycle["campaign_progress"]["T7"] = {"posture": "BOUNDED_PASS"}
