11626362cee8134cf00d8e4be8655ee1d4f1599a
Reed Barrus <reedbarrus@gmail.com>
2026-09-25 17:34:52 -0700

    Test exact materialized-work consumption routing

--- tests/cockpit/test_pressure_justification.py
+++ tests/cockpit/test_pressure_justification.py
@@ -355,6 +355,26 @@ class PressureJustificationTests(unittest.TestCase):
         )
 
 
+    def test_materialized_consumption_is_selected_before_repeated_loop(self):
+        workcycle = workcycle_fixture("AUTO_CONTINUATION_PRESSURE")
+        workcycle["campaign_progress"]["T7"] = {"posture": "BOUNDED_PASS"}
+        result = build_pressure_justification(
+            workcycle=workcycle,
+            horizon_closure=horizon_fixture(),
+            qualification=qualification_fixture(
+                self_moving=[
+                    "MATERIALIZED_ADMITTED_AUTHORITY_CONSUMPTION:UNFROZEN",
+                    "REPEATED_METABOLIC_LOOP:UNFROZEN",
+                ],
+            ),
+        )
+        self.assertEqual(result["pressure_posture"], "JUSTIFIED")
+        self.assertEqual(
+            result["proposed_pressure"],
+            "MATERIALIZED_ADMITTED_AUTHORITY_CONSUMPTION_PRESSURE",
+        )
+
+
     def test_repeated_metabolic_loop_is_selected_after_authority_binding(self):
         workcycle = workcycle_fixture("AUTO_CONTINUATION_PRESSURE")
         workcycle["campaign_progress"]["T7"] = {"posture": "BOUNDED_PASS"}
